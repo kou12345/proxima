6b6f5ba7dea82d4134dcacb84675362835e8cf36
kou12345 <hi.0412.ko.2002@gmail.com>
2024-04-23 23:40:05 +0900

    add type

--- supabase/functions/_shared/supabase.ts
+++ supabase/functions/_shared/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.5";
+import { Database } from "./database.types.ts";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   // Supabase API URL - env var exported by default.
   Deno.env.get("SUPABASE_URL")!,
   // Supabase API ANON KEY - env var exported by default.
