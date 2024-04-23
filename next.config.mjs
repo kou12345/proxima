73344f3bf221b257cba82a1dbb55d1ccad59f7df
kou12345 <hi.0412.ko.2002@gmail.com>
2024-04-23 21:38:09 +0900

    add experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // (Optional) Export as a standalone site
+    // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
+    // output: 'standalone', // Feel free to modify/remove this option
+    experimental: {
+        serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+    },
+};
 
 export default nextConfig;
