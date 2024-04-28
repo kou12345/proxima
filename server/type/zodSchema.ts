2bbe44ff8ed2bb2346715e7f0990b20a0af341b0
kou12345 <hi.0412.ko.2002@gmail.com>
2024-04-28 12:13:13 +0900

    fix import

--- server/type/zodSchema.ts
+++ server/type/zodSchema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { Database } from "../../supabase/functions/_shared/database.types";
+import { Database } from "./database.types";
 
 type MatchDocumentsDot =
   Database["public"]["Functions"]["match_documents_dot"]["Returns"];
