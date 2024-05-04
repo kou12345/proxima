a87f3ebc461ed868b6e9e9468fea1d227d941a32
kou12345 <hi.0412.ko.2002@gmail.com>
2024-05-04 19:04:30 +0900

    wip function calling

--- server/gemini/index.ts
+++ server/gemini/index.ts
@@ -1,18 +1,42 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  FunctionDeclaration,
+  FunctionDeclarationSchemaType,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
 
 if (!process.env.GEMINI_API_KEY) {
   throw new Error("GEMINI_API_KEY is not set");
 }
 
+// Gemini APIのfunction callingのschema
+const functionDeclaration: FunctionDeclaration = {
+  name: "code_review",
+  description: "コードのレビューを行います",
+  parameters: {
+    type: FunctionDeclarationSchemaType.OBJECT,
+    properties: {
+      codeDescription: {
+        type: FunctionDeclarationSchemaType.STRING,
+        description: "コードの目的と概要",
+      },
+      // code: {
+      //   type: FunctionDeclarationSchemaType.STRING,
+      //   description: "コードレビュー対象のコード",
+      // },
+    },
+    description: "コードレビューのパラメータ",
+  },
+};
+
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-export const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+export const model = genAI.getGenerativeModel({
+  model: "gemini-pro",
+  // TODO これが原因でエラーが出ているかも
+  // tools: [{
+  //   functionDeclarations: [functionDeclaration],
+  // }],
+});
+export const MAX_INPUT_TOKENS = 30720;
 // export const model = genAI.getGenerativeModel({
 //   model: "gemini-1.5-pro-latest",
 // });
-export const run = async (prompt: string) => {
-  const result = await model.generateContent(prompt);
-  const response = result.response;
-  const text = response.text();
-
-  return text;
-};
