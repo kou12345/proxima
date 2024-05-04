755863032e70f9059aeeb020fc0e475b24e88e63
kou12345 <hi.0412.ko.2002@gmail.com>
2024-05-04 20:15:01 +0900

    shadcn/ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import { theme } from "@/utils/theme";
+import { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
-// export const metadata: Metadata = {
-//   title: "Proxima",
-// };
+export const metadata: Metadata = {
+  title: "Proxima",
+};
 
 export default function RootLayout({
   children,
@@ -17,15 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
-        <AppRouterCacheProvider options={{ key: "css" }}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="max-w-7xl mx-auto">{children}</div>
-            </div>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto">{children}</div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
