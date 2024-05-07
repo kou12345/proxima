2f7b8533c01684524979fdd76cf76c6aa554dd23
kou12345 <hi.0412.ko.2002@gmail.com>
2024-05-07 23:22:43 +0900

    getTree Changed to delete specific file name and return. and add getContent

--- src/server/github/index.ts
+++ src/server/github/index.ts
@@ -1,4 +1,6 @@
 import { Octokit, RequestError } from "octokit";
+import { ZodError } from "zod";
+import { getTreeResponseSchema } from "../type/tasks/zodSchema";
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
@@ -43,10 +45,57 @@ export const getTree = async () => {
         },
       },
     );
-    console.log(res);
 
-    console.log(res.data.tree);
-    return res.data.tree;
+    const paresedRes = getTreeResponseSchema.parse(res.data.tree);
+
+    // 削除する名前の配列を定義
+    const excludeNames = [".obsidian", ".gitignore"];
+
+    // 削除する名前を含まない要素だけを残す
+    const filteredRes = paresedRes.filter(
+      (element) => !excludeNames.some((name) => element.path.includes(name)),
+    );
+
+    return filteredRes;
+  } catch (error) {
+    if (error instanceof RequestError) {
+      console.log(error.message);
+      console.log(error.status);
+      console.log(error.request);
+      console.log(error.response);
+    } else if (error instanceof ZodError) {
+      console.log(error.errors);
+    } else {
+      throw error;
+    }
+  }
+};
+
+export const getContent = async (path: string) => {
+  try {
+    const res = await octokit.request(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      {
+        owner: OWNER,
+        repo: REPO,
+        path: path,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      },
+    );
+
+    if (Array.isArray(res.data)) {
+      throw new Error("res.data is array");
+    }
+
+    if (res.data.type !== "file") {
+      throw new Error("res.data is not file");
+    }
+
+    // base64でエンコードされているファイルの中身をデコードする
+    const content = Buffer.from(res.data.content, "base64").toString("utf-8");
+    return content;
   } catch (error) {
     if (error instanceof RequestError) {
       console.log(error.message);
