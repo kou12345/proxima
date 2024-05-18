2ffa0dc0cb723d9d7c9c9a9fa73c8d09d2db7145
kou12345 <hi.0412.ko.2002@gmail.com>
2024-05-18 19:24:43 +0900

    Databaseの型を追加

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,12 +1,13 @@
 "use server";
 
+import { Database } from "@/server/type/database.types";
 import { type CookieOptions, createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export async function createClient() {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
