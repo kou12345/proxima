3f397fbb5f955761588b3140b03610c02140a8c2
kou12345 <hi.0412.ko.2002@gmail.com>
2024-06-04 15:40:10 +0900

    create table memos

--- supabase/migrations/0000_happy_harry_osborn.sql
+++ supabase/migrations/0000_happy_harry_osborn.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "memos" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid NOT NULL,
-	"title" text DEFAULT ''Untitled'' NOT NULL,
+	"title" text DEFAULT 'Untitled' NOT NULL,
 	"content" text,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now(),
