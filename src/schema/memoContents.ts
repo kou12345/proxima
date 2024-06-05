85283f1ca3751cd8d308cd0e373ae8963e6ace39
kou12345 <hi.0412.ko.2002@gmail.com>
2024-06-05 15:27:41 +0900

    fix table, migration

--- src/schema/memoContents.ts
+++ src/schema/memoContents.ts
@@ -1,10 +1,12 @@
 import { sql } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { memos } from "./memos";
 
-export const memos = pgTable("memos", {
+export const memoContents = pgTable("memo_contents", {
   id: uuid("id").default(sql`gen_random_uuid()`).primaryKey(),
+  memoId: uuid("memo_id").notNull().references(() => memos.id),
   userId: uuid("user_id").notNull(),
-  title: text("title").notNull().default("Untitled"),
+  content: text("content"),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
