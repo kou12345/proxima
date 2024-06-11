08458995968a9be101fb0163683adeb744504e18
kou12345 <hi.0412.ko.2002@gmail.com>
2024-06-11 15:15:45 +0900

    fix タイトルを入力するように変更

--- src/app/memos/NewMemoButton.tsx
+++ src/app/memos/NewMemoButton.tsx
@@ -1,13 +1,35 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { newMemo } from "@/server/actions/newMemo";
 
 export const NewMemoButton = () => {
-  const router = useRouter();
-  const onClickNewMemo = async () => {
-    router.push("/memos/new");
-  };
-
-  return <Button onClick={onClickNewMemo}>new memo</Button>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>New Memo</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>タイトルを入力して下さい</DialogTitle>
+        </DialogHeader>
+        <form action={newMemo}>
+          <Input id="title" name="title" className="mb-4" required />
+          <div className="flex justify-end">
+            <Button type="submit" className="w-1/4">
+              作成
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 };
