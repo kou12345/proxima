d547b08423055e68500bf29194ad12e6004910df
kou12345 <hi.0412.ko.2002@gmail.com>
2024-06-11 15:16:33 +0900

    add comment

--- src/schema/memos.ts
+++ src/schema/memos.ts
@@ -4,6 +4,7 @@ import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 export const memos = pgTable("memos", {
   id: uuid("id").default(sql`gen_random_uuid()`).primaryKey(),
   userId: uuid("user_id").notNull(),
+  // unique制約つけても良いな
   title: text("title").notNull().default("Untitled"),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
