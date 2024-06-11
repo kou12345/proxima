6515c9b426d1810cb8395ec80b25253641af4f8d
kou12345 <hi.0412.ko.2002@gmail.com>
2024-06-11 15:18:47 +0900

    諸々修正

--- src/server/actions/memo.ts
+++ src/server/actions/memo.ts
@@ -3,10 +3,13 @@
 import { createClient } from "@/utils/supabase/server";
 import { db } from "../db";
 import { memos } from "@/schema/memos";
-import { desc, eq, sql } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { memoContents } from "@/schema/memoContents";
+import { revalidatePath } from "next/cache";
+import "server-only";
 
 export const getMemos = async () => {
+  console.log(new Date());
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) {
@@ -17,37 +20,32 @@ export const getMemos = async () => {
   const res = await db.select({
     id: memos.id,
     title: memos.title,
-  }).from(memos).where(eq(memos.userId, user.id));
+    content: memoContents.content,
+  }).from(memos).where(eq(memos.userId, user.id)).leftJoin(
+    memoContents,
+    eq(memos.id, memoContents.memoId),
+  );
+
+  console.log(res);
+
+  revalidatePath("/memos");
 
   return res;
 };
 
-export const getMemoContentByTitle = async (title: string) => {
+export const getMemoContentsByTitle = async (title: string) => {
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) {
     throw new Error("Unauthorized");
   }
 
-  // titleが”new"の場合はデータがないため、title, contentを空にして返す
-  if (title === "new") {
-    const latestId = await db.select({
-      id: memos.id,
-    }).from(memos).orderBy(desc(memos.createdAt)).limit(1);
-
-    return {
-      id: latestId[0].id,
-      title: "",
-      content: "",
-    };
-  }
-
-  // const result = await db.select().from(users).innerJoin(pets, eq(users.id, pets.ownerId))
   const memo = await db.select({
     id: memos.id,
     title: memos.title,
     content: memoContents.content,
-  }).from(memos).where(eq(memos.title, title)).innerJoin(
+    createdAt: memoContents.createdAt,
+  }).from(memos).where(eq(memos.title, title)).leftJoin(
     memoContents,
     eq(memos.id, memoContents.memoId),
   );
@@ -58,7 +56,40 @@ export const getMemoContentByTitle = async (title: string) => {
 
   console.log("memo : ", memo);
 
-  return memo[0];
+  return {
+    id: memo[0].id,
+    title: memo[0].title,
+    contents: memo.map((item) => (
+      {
+        content: item.content || "",
+        createdAt: item.createdAt,
+      }
+    )),
+  };
+};
+
+export const createMemo = async (title: string) => {
+  const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  const memo = await db.insert(memos).values({
+    userId: user.id,
+    title,
+  }).returning({
+    id: memos.id,
+    title: memos.title,
+  });
+
+  await db.insert(memoContents).values({
+    memoId: memo[0].id,
+    userId: user.id,
+    content: "",
+  });
+
+  return memo;
 };
 
 export const updateMemoTitle = async (id: string, title: string) => {
@@ -112,14 +143,31 @@ export const updateMemoTitle = async (id: string, title: string) => {
   // revalidateTag(`/memos/${id}`);
 };
 
-export const updateMemoContent = async (id: string, content: string) => {
+// export const updateMemoContent = async (id: string, content: string) => {
+//   const supabase = await createClient();
+//   const { data: { user } } = await supabase.auth.getUser();
+//   if (!user) {
+//     throw new Error("Unauthorized");
+//   }
+
+//   await db.update(memos).set({ content }).where(eq(memos.id, id));
+
+//   // revalidateTag(`/memos/${id}`);
+// };
+
+export const createMemoContent = async (
+  memoId: string,
+  content: string,
+) => {
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) {
     throw new Error("Unauthorized");
   }
 
-  await db.update(memos).set({ content }).where(eq(memos.id, id));
-
-  // revalidateTag(`/memos/${id}`);
+  await db.insert(memoContents).values({
+    memoId: memoId,
+    userId: user.id,
+    content: content,
+  });
 };
