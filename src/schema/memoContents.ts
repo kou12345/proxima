71858a94237b7869492a9292d7c48d8ad18a3fe9
kou12345 <hi.0412.ko.2002@gmail.com>
2024-06-12 13:47:57 +0900

    migrate

--- src/schema/memoContents.ts
+++ src/schema/memoContents.ts
@@ -6,7 +6,7 @@ export const memoContents = pgTable("memo_contents", {
   id: uuid("id").default(sql`gen_random_uuid()`).primaryKey(),
   memoId: uuid("memo_id").notNull().references(() => memos.id),
   userId: uuid("user_id").notNull(),
-  content: text("content"),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
--- src/schema/memos.ts
+++ src/schema/memos.ts
@@ -5,7 +5,7 @@ export const memos = pgTable("memos", {
   id: uuid("id").default(sql`gen_random_uuid()`).primaryKey(),
   userId: uuid("user_id").notNull(),
   // unique制約つけても良いな
-  title: text("title").notNull().default("Untitled"),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
+  title: text("title").default("Untitled").notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
